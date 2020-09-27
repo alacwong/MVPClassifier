96b30db65b36ae32b9b938be80078b66bfb54289
alacwong <mr6902spades@gmail.com>
2020-09-26 20:44:10 -0400

    updated scripts to generate data for all players

--- data/scripts/generate_data.py
+++ data/scripts/generate_data.py
@@ -1,11 +1,8 @@
 # script to scrape mvp data
-from scripts.panda_util import get_df
+from scripts.panda_util import get_df, get_season_df
 from scripts.numpy_util import generator_vector
 import time
 import pandas as pd
-import numpy as np
-from typing import List
-import psycopg2
 
 
 def get_data():
@@ -16,16 +13,35 @@ def get_data():
 
     start = time.time()
 
-    for x in range(1980, 2020):
+    for year in range(1980, 2020):
         url = (
-            'https://www.basketball-reference.com/awards/awards_' + str(x) + '.html',
-            'https://www.basketball-reference.com/leagues/NBA_' + str(x) + '_standings.html'
+            'https://www.basketball-reference.com/awards/awards_' + str(year) + '.html',
+            'https://www.basketball-reference.com/leagues/NBA_' + str(year) + '_standings.html'
         )
         df = get_df(url)
-        df.to_csv(str(x) + '.csv')
+        df.to_csv(str(year) + '.csv')
 
     end = time.time()
     print('Loaded  elements in %f s' % (end - start))
+
+
+def get_season_data():
+    """
+    generate data for players among entire season
+    :return:
+    """
+
+    start = time.time()
+    for year in range(1980, 2020):
+        url = (
+            'https://www.basketball-reference.com/leagues/NBA_' + str(year) + '_per_game.html',
+            'https://www.basketball-reference.com/leagues/NBA_' + str(year) + '_standings.html',
+            'https://www.basketball-reference.com/leagues/NBA_' + str(year) + '_advanced.html'
+        )
+        df = get_season_df(url)
+        df.to_csv('../all_csv/' + str(year) + '.csv')
+    end = time.time()
+    print('Loaded elements in %f s' % (end - start))
 
 
 def load_data(files):
@@ -40,39 +56,5 @@ def load_data(files):
 
 
 if __name__ == "__main__":
-    get_data()
-
-# if __name__ == "__main__":
-#     url_data = [
-#         (
-#             'https://www.basketball-reference.com/awards/awards_' + str(x) + '.html',
-#             'https://www.basketball-reference.com/leagues/NBA_' + str(x) + '_standings.html'
-#         )
-#         for x in range(1980, 2020)  # include era of 3 point
-#     ]
-#     # data = get_df(url_data)
-#     # add to postgresql database
-#
-#     # connect to postgres
-#     connection = psycopg2.connect(
-#         user="dbuser",
-#         password="password",
-#         host="localhost",
-#         port="5432",
-#         database="training"
-#     )
-#     print(connection.get_dsn_parameters())
-#     cur = connection.cursor()
-#     sql = """CREATE TABLE(""" + """);"""
-#
-#
-#
-#     cur.execute(
-#         sql
-#     )
-#     connection.commit()
-#
-#     cur.execute("""SELECT table_name FROM information_schema.tables
-#            WHERE table_schema = 'public'""")
-#     for table in cur.fetchall():
-#         print(table)
+    # get_data()
+    get_season_data()
