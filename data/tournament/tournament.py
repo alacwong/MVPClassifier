1c080c486f9dd1a800bda2b0c23014f272cff072
alacwong <mr6902spades@gmail.com>
2021-02-06 13:58:50 -0500

    reconfigure tournament

--- data/tournament/tournament.py
+++ data/tournament/tournament.py
@@ -1,36 +1,46 @@
 # use tournament algorithm to find mvp given an list of player stats from an nba regular season
 from .model import Model
 import numpy as np
+from collections import deque
+from scripts.numpy_util import normalize_vector
 
 
 class Tournament:
 
-    def __init__(self, data, model: Model):
-        self.data = data
+    def __init__(self, players, statistics, model: Model):
+        self.player = players
+        self.statistics = statistics
         self.model = model
 
-    def simulate(self):
-        """Simulate tournament on regular season players to get mvp"""
-        keys = [d for d in self.data]
+    def simulate(self, k=5):
+        """
+        Simulate tournament on regular season players get
+        top k mvp candidates
+        """
 
-        mvp = self.simulate_helper(keys)
-        print(mvp[1])
+        # iterate through all players
+        q = deque([i for i in range(len(self.statistics))])
 
-    def simulate_helper(self, keys):
-        """recursive algorithm to help simulate tournament"""
-
-        if len(keys) == 1:
-            return keys
-        elif len(keys) == 2:
-            vector = np.concatenate(self.data[keys[0][1]], self.data[keys[1][1]])
-            if self.model.evaluate(vector) > 0.5:
-                return keys[0]
+        while q:
+            advance = deque([])
+            if len(q) >= 2:
+                i = q.popleft()
+                j = q.popleft()
+                stat = normalize_vector(self.statistics[i], self.statistics[j])
+                pred = self.model.predict(stat)[0]
+                if pred > 0.5:
+                    advance.append(i)
+                else:
+                    advance.append(j)
             else:
-                return keys[1]
-        else:
-            return self.simulate_helper(
-                [
-                    self.simulate_helper(keys[:len(keys)/2]),
-                    self.simulate_helper(keys[len(keys)/2:])
-                ]
-            )
+                i = q.popleft()
+                advance.append(i)
+            if len(advance) > 1:
+                q = advance
+            else:
+                mvp = advance.popleft()
+
+        print(self.player[mvp])
+
+
+
