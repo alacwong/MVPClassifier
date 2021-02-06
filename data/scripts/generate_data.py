a77efb3f92cce439214658671b9d9dcaa04951bf
alacwong <mr6902spades@gmail.com>
2021-02-06 02:26:31 -0500

    feat: add player scraping

--- data/scripts/generate_data.py
+++ data/scripts/generate_data.py
@@ -3,6 +3,10 @@ from scripts.panda_util import get_df, get_season_df
 from scripts.numpy_util import generator_vector, assign_vector
 import time
 import pandas as pd
+from bs4 import BeautifulSoup
+from requests import get
+import json
+from utils.bs4_utils import get_filter
 
 
 def get_data():
@@ -62,6 +66,78 @@ def load_raw(files):
     for file in files:
         seasons[file] = assign_vector(pd.read_csv(file))
     return seasons
+
+
+def scrape_players():
+    """
+    Scrape players
+    :return:
+    """
+
+    # In prod use config file to load year
+    link = 'https://www.basketball-reference.com/leagues/NBA_2021_totals.html'
+    advanced = 'https://www.basketball-reference.com/leagues/NBA_2021_advanced.html'
+    team_summary = 'https://www.basketball-reference.com/leagues/NBA_2021.html'
+
+    response = get(link)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    table_data = [t.text for t in soup.find_all('td')]
+    header_data = [h.text for h in soup.find_all('th')][1:30]
+    all_players = []
+
+    win_share = {}
+
+    # get player statistics
+    for i in range(0, len(table_data), len(header_data)):
+        player = {}
+        for j in range(len(header_data)):
+            player[header_data[j]] = table_data[i + j]
+        if player['Tm'] != 'TOT':
+            all_players.append(player)
+            win_share[(player['Player'], player['Tm'], player['Pos'])] = player
+
+    # get advanced stats (win-share)
+    response = get(advanced)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    advanced_header = [h.text for h in soup.find_all('th')][1:29]
+    advanced_stats = [s.text for s in soup.find_all('td')]
+
+    for i in range(0, len(advanced_stats), len(advanced_header)):
+        stats = {}
+        for j in range(len(advanced_header)):
+            stats[advanced_header[j]] = advanced_stats[i + j]
+
+        if stats['Tm'] != 'TOT':
+            win_share[(stats['Player'], stats['Tm'], stats['Pos'])]['WS'] = stats['WS']
+
+    # get team statistics
+    response = get(team_summary)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    with open('../../assets/teams.json') as f:
+        tm_team = json.load(f)
+    team_tm = {tm_team[tm]: tm for tm in tm_team}
+
+    tm_pct = {}
+    tm_table = list(soup.find_all('table'))
+    east_func, west_func = get_filter()
+    ecf = list(filter(east_func, tm_table))[0]
+    wcf = list(filter(west_func, tm_table))[0]
+    east = [tm_name.text for tm_name in ecf.find_all('a')]
+    west = [tm_name.text for tm_name in wcf.find_all('a')]
+    east_pct = [pct.text for pct in ecf.find_all('td', attrs={"data-stat": 'win_loss_pct'})]
+    west_pct = [pct.text for pct in wcf.find_all('td', attrs={"data-stat": 'win_loss_pct'})]
+
+    for i in range(len(east)):
+        tm_pct[team_tm[east[i]]] = east_pct[i]
+
+    for i in range(len(west)):
+        tm_pct[team_tm[west[i]]] = west_pct[i]
+
+    for player in all_players:
+        player['Tm'] = tm_pct[player['Tm']]
+
+    print(all_players)
+    print(f'Loaded {len(all_players)} player statistics.')
 
 
 if __name__ == "__main__":
