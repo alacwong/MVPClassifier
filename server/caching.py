b04c3e167de27efed2bed3b0c05ae0f09a5067bb
alacwong <mr6902spades@gmail.com>
2021-02-26 01:38:00 -0500

    load url

--- server/caching.py
+++ server/caching.py
@@ -1,11 +1,14 @@
 from redis import Redis
 import redis
 import os
+import json
 
 # dev
 # r = redis.from_url('redis://localhost:6380/1')
 
 # prod
 # r = Redis(host='redis', port=6379)
-print(os.environ)
-r = redis.from_url(os.environ['REDISCLOUD_URL'])
+with open('config.json', 'r') as f:
+    uri = json.load(f)['heroku']
+
+r = redis.from_url(uri)
