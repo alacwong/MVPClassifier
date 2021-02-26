de27a74845a76dfd725485891af64da27040e856
alacwong <mr6902spades@gmail.com>
2021-02-26 00:47:28 -0500

    fix redis

--- server/caching.py
+++ server/caching.py
@@ -1,8 +1,10 @@
 from redis import Redis
 import redis
+import os
 
 # dev
 # r = redis.from_url('redis://localhost:6380/1')
 
 # prod
-r = Redis(host='redis', port=6379)
+# r = Redis(host='redis', port=6379)
+r = redis.from_url(os.environ['REDISCLOUD_URL'])
